8c97e34513d21c37fbb2be4d16a93a9576ac0f7a
mahama saani zakaria <zakariamahamasaani@gmail.com>
2024-11-01 15:27:41 +0000

    add Django admin dashboard customization

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # Display the specified fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Add filter options for publication year
+    list_filter = ('publication_year',)
+
+    # Enable search functionality for title and author fields
+    search_fields = ('title', 'author')
+
+# Register the Book model with the custom BookAdmin configuration
+admin.site.register(Book, BookAdmin)
+
