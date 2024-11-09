0c4eddbd07a3e890538dfaa2c4f5abf75002faec
MSZAKARIA1 <61415685+MSZAKARIA1@users.noreply.github.com>
2024-11-09 14:34:10 +0000

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 # adding a function-based view that retrieves all books and displays their titles and authors:
 from django.shortcuts import render
 from .models import Book
+from django.views.generic.detail import DetailView
 from django.views.generic import DetailView
 from .models import Library
 
